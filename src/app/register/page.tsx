6cb8fba96f61b5397a70a0898d732bb4186b4e73
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-10-08 03:30:33 +0800

    Feat #19: Create the sign up page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,14 @@
+import RegistrationForm from "@/components/RegistrationForm";
+// import BottomNav from "@/components/BottomNav";
+import React from "react";
+
+const RegistrationPage = () => {
+  return (
+    <main className="w-full h-fit flex flex-col items-center justify-start overflow-y-auto bg-[#03022D]">
+      <RegistrationForm />
+      {/* <BottomNav /> */}
+    </main>
+  );
+};
+
+export default RegistrationPage;
