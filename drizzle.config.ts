45ec6f35dcd03f9e22d006e98d3fe0d088f397cc
Saga-sanga <vulcan248@gmail.com>
2023-10-07 23:00:30 +0530

    chore: setup drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  driver: 'mysql2',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL as string,
+  },
+} satisfies Config;
