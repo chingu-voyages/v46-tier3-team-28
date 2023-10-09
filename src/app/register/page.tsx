e4f295b0b2711f74b2a798dda096b4718eab2302
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-10-09 20:54:12 +0800

    Fix: Add the BottomNav to the registration page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,12 @@
 import RegistrationForm from "@/components/RegistrationForm";
-// import BottomNav from "@/components/BottomNav";
+import BottomNav from "@/components/BottomNav";
 import React from "react";
 
 const RegistrationPage = () => {
   return (
     <main className="w-full h-fit flex flex-col items-center justify-start overflow-y-auto bg-[#03022D]">
       <RegistrationForm />
-      {/* <BottomNav /> */}
+      <BottomNav />
     </main>
   );
 };
