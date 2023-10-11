e22a60e4c95bc96ab1b51472c30a3dae76e3b6a0
julichow <julichow94@gmail.com>
2023-10-11 21:37:27 +0800

    feat: Create landing page and navigation bar

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,17 @@
+import Feed from "./Feed"
+
+function LandingPage() {
+  return (
+    <section>
+      <h1 className="text-center">Discover the easiest way to save and organize your favorite URLs with our intuitive bookmarking app
+      </h1>
+      <p className="text-center">Capture, Categorize, and Share with Ease
+      </p>
+
+      <Feed/>
+     
+    </section>
+  )
+}
+
+export default LandingPage
