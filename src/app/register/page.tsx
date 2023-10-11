6ecf1ff60b77dbd808d4109dab27f4f5bfcec7f1
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-10-11 20:28:29 +0800

    fix #51: Change the registration page design

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,12 @@
 import RegistrationForm from "@/components/RegistrationForm";
-import BottomNav from "@/components/BottomNav";
+
 import React from "react";
 
 const RegistrationPage = () => {
   return (
-    <main className="w-full h-fit flex flex-col items-center justify-start overflow-y-auto bg-[#03022D]">
+    <main className="w-full h-fit min-h-screen flex items-center justify-start overflow-y-auto flex-col py-32 bg-[#FAFAFA]">
       <RegistrationForm />
-      <BottomNav />
+  
     </main>
   );
 };
