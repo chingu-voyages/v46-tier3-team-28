38bb0bc3da0d5fa65c4e1689ad670a3c98077386
Saga-sanga <vulcan248@gmail.com>
2023-10-13 17:37:37 +0530

    feat: add endpoint for creating new collection

--- src/lib/validators/register.ts
+++ src/lib/validators/register.ts
@@ -5,3 +5,12 @@ export const registerSchema = z.object({
   name: z.string().nullish(),
   password: z.string().min(8),
 });
+
+export const collectionCreateSchema = z.object({
+  // id: z.string(),
+  userId: z.string(),
+  // createdAt: z.date().nullish(),
+  // updatedAt: z.date().nullish(),
+  title: z.string(),
+  // private: z.boolean().nullish(),
+});
