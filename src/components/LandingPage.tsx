8c4776e7e870a1dc99adcab9a1db482ee7cf9868
julichow <julichow94@gmail.com>
2023-10-26 17:40:52 +0800

    updated footer alignment

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,18 +1,18 @@
 import Feed from "./Feed";
-import Footer from "./Footer"
+import Footer from "./Footer";
 
 const LandingPage = () => {
   return (
-    <section >
-      <div >
-        <h1 className="text-center font-extrabold text-2xl text-white">Discover the easiest way to save and organize your favorite URLs with our intuitive bookmarking app</h1>
-        <p className="text-center font-semibold text-lg text-white">Capture, Categorize, and Share with Ease</p>
-        <Feed/>
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </section>
+    <div className="flex flex-col min-h-screen"> 
+      <section className="flex-grow pb-4">
+        <div>
+          <h1 className="text-center font-extrabold text-2xl text-white">Discover the easiest way to save and organize your favorite URLs with our intuitive bookmarking app</h1>
+          <p className="text-center font-semibold text-lg text-white">Capture, Categorize, and Share with Ease</p>
+          <Feed />
+        </div>
+      </section>
+      <Footer />
+    </div>
   );
 }
 
