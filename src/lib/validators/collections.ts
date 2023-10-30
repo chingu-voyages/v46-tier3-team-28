80ae1cb533010ab604097dd759a9c6857267f796
Saga-sanga <vulcan248@gmail.com>
2023-10-30 19:43:45 +0530

    feat: create get and post endpoint for items

--- src/lib/validators/collections.ts
+++ src/lib/validators/collections.ts
@@ -8,16 +8,27 @@ export const routeContextSchema = z.object({
 
 export const collectionCreateSchema = z.object({
   // id: z.string(),
+<<<<<<< HEAD
   // userId: z.string(),
   // createdAt: z.date().nullish(),
   // updatedAt: z.date().nullish(),
   title: z.string(),
   description: z.string(),
   private: z.string().optional(),
+=======
+  userId: z.string(),
+  // createdAt: z.date().nullish(),
+  // updatedAt: z.date().nullish(),
+  title: z.string(),
+  // private: z.boolean().nullish(),
+>>>>>>> a85024e (feat: create get and post endpoint for items)
 });
 
 export const collectionPatchSchema = z.object({
   title: z.string().optional(),
   private: z.boolean().optional(),
+<<<<<<< HEAD
   description: z.string(),
+=======
+>>>>>>> a85024e (feat: create get and post endpoint for items)
 });
