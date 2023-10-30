b94fb3276b53fbb31a9854ee5c4d58865900fb66
Saga-sanga <vulcan248@gmail.com>
2023-10-30 18:54:31 +0530

    feat: add endopoint to handle deletion of collection

--- src/app/api/collections/[collectionId]/route.ts
+++ src/app/api/collections/[collectionId]/route.ts
@@ -1,10 +1,10 @@
+import { db } from '@/db';
+import { collections } from '@/db/schema';
+import { collectionPatchSchema } from '@/lib/validators/register';
+import { eq, sql } from 'drizzle-orm';
 import { getServerSession } from 'next-auth';
 import z from 'zod';
 import { authOptions } from '../../auth/[...nextauth]/authOptions';
-import { db } from '@/db';
-import { eq, sql } from 'drizzle-orm';
-import { collections } from '@/db/schema';
-import { collectionPatchSchema } from '@/lib/validators/register';
 
 // Add PATCH and DELETE methods for unique collection id
 const routeContextSchema = z.object({
@@ -17,7 +17,7 @@ export async function PATCH(req: Request, context: z.infer<typeof routeContextSc
   try {
     // validate route params
     const { params } = routeContextSchema.parse(context);
-    console.log('here');
+    console.log('here', params);
     // check if user has access to collection
     if (!(await verifyIfUserHasAccess(params.collectionId))) {
       return new Response(null, { status: 403 });
@@ -46,14 +46,35 @@ export async function PATCH(req: Request, context: z.infer<typeof routeContextSc
   }
 }
 
-export async function DELETE(req: Request) {}
+export async function DELETE(req: Request, context: z.infer<typeof routeContextSchema>) {
+  try {
+    // Validate params
+    const { params } = routeContextSchema.parse(context);
+
+    // Check if user has access
+    if (!(await verifyIfUserHasAccess(params.collectionId))) {
+      return new Response(null, { status: 403 });
+    }
+
+    // Delete collection
+    await db.delete(collections).where(eq(collections.id, parseInt(params.collectionId)));
+
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 });
+    }
+
+    return new Response(null, { status: 500 });
+  }
+}
 
 async function verifyIfUserHasAccess(collectionId: string) {
   const session = await getServerSession(authOptions);
   const result = await db
     .select({ count: sql<number>`count(*)` })
     .from(collections)
-    .where(eq(collections.id, parseInt(collectionId)));
+    .where(sql`${collections.id} = ${parseInt(collectionId)} and ${collections.userId} = ${session?.user.id}`);
 
   const { count } = result[0];
   return count > 0;
