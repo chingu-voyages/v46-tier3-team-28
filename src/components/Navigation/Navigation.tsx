a50070683a2b6ac33fdd2d9a2e48b2c3df39d403
Saga-sanga <vulcan248@gmail.com>
2023-11-06 16:11:54 +0530

    feat: add nav for dashboard

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,30 @@
 'use client';
 import { Session } from 'next-auth';
 import { signIn, signOut } from 'next-auth/react';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { nameToInitials } from '@/lib/utils';
+import { UserOptions } from './UserOptions';
 
 export default function Navigation({ session }: { session: Session }) {
   return (
-    <div>
-      {session ? (
-        <div className="flex space-x-8">
-          <span>
-            <h2>Welcome! {session?.user?.name ?? session?.user?.email ?? 'friend'}</h2>
-          </span>
-          <button onClick={() => signOut({ callbackUrl: '/' })}>Sign Out</button>
+    <div className="bg-background border-b">
+      <div className="flex flex-row justify-between h-16 items-center max-w-screen-xl mx-auto">
+        {/* {session ? (
+          <div className="flex space-x-8">
+            <span>
+              <h2>Welcome! {session?.user?.name ?? session?.user?.email ?? 'friend'}</h2>
+            </span>
+            <button onClick={() => signOut({ callbackUrl: '/' })}>Sign Out</button>
+          </div>
+        ) : (
+          <button onClick={() => signIn()}>Sign in</button>
+        )} */}
+        <div>
+          <Link href="/dashboard">PagePocket</Link>
         </div>
-      ) : (
-        <button onClick={() => signIn()}>Sign in</button>
-      )}
+        <UserOptions session={session} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navigation/UserOptions.tsx
@@ -0,0 +1,44 @@
+import { nameToInitials } from '@/lib/utils';
+import { Session } from 'next-auth';
+import { LuLogOut, LuSettings } from 'react-icons/lu';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import { signOut } from 'next-auth/react';
+
+export function UserOptions({ session }: { session: Session }) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="cursor-pointer">
+          <AvatarImage src={session.user.image ?? undefined} alt={session.user.name ?? undefined} />
+          <AvatarFallback>{nameToInitials(session.user.name ?? '')}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-2" align="end">
+        <DropdownMenuLabel>
+          <p className="truncate">{session.user.name}</p>
+          <p className="truncate text-sm font-normal text-muted-foreground">{session.user.email}</p>
+        </DropdownMenuLabel>
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <div className="flex items-center space-x-2">
+              <LuSettings /> <span>Settings</span>
+            </div>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => signOut({ callbackUrl: '/' })}>
+            <div className="flex items-center space-x-2">
+              <LuLogOut /> <span>Logout</span>
+            </div>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
