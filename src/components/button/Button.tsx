50df8b1689b5fdf4fab9ed642870d0abf74dac6d
Victor Fedorov <vpfedorov25@gmail.com>
2023-11-07 20:38:44 +0700

    Feat: Added button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type ButtonProps = {
+    text: string;
+    onClick: () => void;
+}
+export const Button: React.FC<ButtonProps> = (props) => {
+    return (
+        <button
+            className="bg-[#633CFF] text-white flex flex-col justify-center items-center font-medium py-3 px-3 mt-6 rounded-md hover:bg-opacity-80 cursor-pointer transition-all duration-300"
+            onClick={props.onClick}
+        >
+            {props.text}
+        </button>
+    );
+};
