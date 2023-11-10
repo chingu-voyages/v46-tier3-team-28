bac52b387b90e737c510e58b95171a1f9bdeb238
Saga-sanga <vulcan248@gmail.com>
2023-11-10 11:30:38 +0530

    fix: login form error

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,70 +1,69 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoginForm } from '@/hooks/useLoginForm';
 import { InputField } from '@/components/InputField/InputField';
 import OAuthButtons from '@/components/OAuthButtons/OAuthButtons';
-import { Logo} from "@/components/InputField/LogoSVG";
+import { Logo } from '@/components/InputField/LogoSVG';
 import { LuLoader2 } from 'react-icons/lu';
 import { Button } from '../ui/button';
 import Link from 'next/link';
 
-
 const LoginForm = () => {
+  const [loading, setLoading] = useState(false);
   const { email, setEmail, password, setPassword, validateLogin } = useLoginForm();
 
   return (
-      <form onSubmit={validateLogin} className="h-fit w-fit bg-transparent dark:bg-black">
-        <div className="w-full h-fit flex flex-col justify-center items-center pb-10">
-          <Logo /> {/* SVG Component */}
-        </div>
-
+    <form onSubmit={validateLogin} className="h-fit w-fit bg-transparent dark:bg-black">
+      <div className="w-full h-fit flex flex-col justify-center items-center pb-10">
+        <Logo /> {/* SVG Component */}
+      </div>
 
       <fieldset className="flex flex-col gap-10 w-screen md:w-[476px] lg:w-[476px] xl:w-[476px] p-[5%] md:p-10 lg:p-10 xl:p-10 white rounded-lg bg-white">
-          <div className="flex flex-col gap-2">
-              <h2 className="text-[#333333] text-3xl font-bold text-center md:text-left lg:text-left xl:text-left leading-[48px]">
-                  Login
-              </h2>
-              <p className="text-[#737373] text-center text-md:text-left lg:text-left xl:text-left">
-                  Enter your credentials below to login
-              </p>
-          </div>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-[#333333] text-3xl font-bold text-center md:text-left lg:text-left xl:text-left leading-[48px]">
+            Login
+          </h2>
+          <p className="text-[#737373] text-center text-md:text-left lg:text-left xl:text-left">
+            Enter your credentials below to login
+          </p>
+        </div>
         <InputField
-            label="Email"
-            type="email"
-            name="email"
-            placeholder="sample@gmail.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+          label="Email"
+          type="email"
+          name="email"
+          placeholder="sample@gmail.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <InputField
-            label="Password"
-            type="password"
-            name="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+          label="Password"
+          type="password"
+          name="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-          <p className="w-full flex flex-row justify-end cursor-pointer">Forgot password?</p>
-          <Button className="bg-[#633CFF] hover:bg-[#633CFF]-80 text-base h-12" type="submit" disabled={loading}>
-            {loading && <LuLoader2 className="animate-spin mr-2" />}
-            Login
-          </Button>
-          <fieldset className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 place-items-center">
-              <div className="h-[1px] w-2/5 md:w-full lg:w-full xl:w-full bg-[#D9D9D9] flex self-center"></div>
-              <h5 className="text-[#737373] w-full text-center col-span-2">OR CONTINUE WITH</h5>
-              <div className="h-[1px] w-1/5 md:w-full lg:w-full xl:w-full bg-[#D9D9D9] flex self-center"></div>
-          </fieldset>
-          <OAuthButtons loading={loading} setLoading={setLoading} />
-          <p className="text-[#737373] text-center">
-              Don't have an account?{' '}
-              <Link className="text-[#633CFF]" href="/register">
-                  Create one
-              </Link>
-          </p>
-          </fieldset>
-      </form>
+        <p className="w-full flex flex-row justify-end cursor-pointer">Forgot password?</p>
+        <Button className="bg-[#633CFF] hover:bg-[#633CFF]-80 text-base h-12" type="submit" disabled={loading}>
+          {loading && <LuLoader2 className="animate-spin mr-2" />}
+          Login
+        </Button>
+        <fieldset className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 place-items-center">
+          <div className="h-[1px] w-2/5 md:w-full lg:w-full xl:w-full bg-[#D9D9D9] flex self-center"></div>
+          <h5 className="text-[#737373] w-full text-center col-span-2">OR CONTINUE WITH</h5>
+          <div className="h-[1px] w-1/5 md:w-full lg:w-full xl:w-full bg-[#D9D9D9] flex self-center"></div>
+        </fieldset>
+        <OAuthButtons loading={loading} setLoading={setLoading} />
+        <p className="text-[#737373] text-center">
+          Don't have an account?{' '}
+          <Link className="text-[#633CFF]" href="/register">
+            Create one
+          </Link>
+        </p>
+      </fieldset>
+    </form>
   );
 };
 
--- src/hooks/useLoginForm.ts
+++ src/hooks/useLoginForm.ts
@@ -2,33 +2,36 @@
 import { useState } from 'react';
 import { signIn } from 'next-auth/react';
 import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
 
 export const useLoginForm = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
 
-    const validateLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const validateLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        const response = await signIn('credentials', {
-            email,
-            password,
-            redirect: false,
-        });
+    const response = await signIn('credentials', {
+      email,
+      password,
+      redirect: false,
+    });
 
-        if (response?.ok) {
-            toast.success('Successful login!');
-            window.location.href = '/dashboard';
-        } else {
-            toast.error('Incorrect email or password!');
-        }
-    };
+    if (response?.ok) {
+      toast.success('Successful login!');
+      // window.location.href = '/dashboard';
+      router.push('/dashboard');
+    } else {
+      toast.error('Incorrect email or password!');
+    }
+  };
 
-    return {
-        email,
-        setEmail,
-        password,
-        setPassword,
-        validateLogin,
-    };
+  return {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    validateLogin,
+  };
 };
